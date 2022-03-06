fcede894f8d8eac0b5764fb61dd0b2ae58d8ca3c
Tanmay Mahendra Kothale <89433097+tanmay-mk@users.noreply.github.com>
2022-03-06 15:38:09 -0700

    runner attempt 6

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -9,7 +9,7 @@ LDD_VERSION = ad4ec466669a82e8a1a6f72bcacc79b981bc2ec7
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-tanmay-mk123456789.git
+LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-tanmay-mk.git
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
 
