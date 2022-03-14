3225c12f82ce5872bd2cba1bb8bc10654e603620
Tanmay Mahendra Kothale <89433097+tanmay-mk@users.noreply.github.com>
2022-03-13 19:45:10 -0600

    commit for runner

---
+++ base_external/package/aesd-char/aesd-char.mk
@@ -0,0 +1,30 @@
+##############################################################
+#
+# AESD-CHAR-ASSIGNMENTS
+#
+##############################################################
+
+#TODO: Fill up the contents below in order to reference your assignment 3 git contents
+AESD_CHAR_VERSION = 8507c8e8ab643c84163e4a876f32a67ff27a7aff #commit version number for A8 from A3 repo
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+AESD_CHAR_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-tanmay-mk.git
+AESD_CHAR_SITE_METHOD = git
+AESD_CHAR_GIT_SUBMODULES = YES
+
+AESD_CHAR_MODULE_SUBDIRS = aesd-char-driver/
+
+# TODO adding all the dependencies.
+define AESD_CHAR_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin
+	
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment8/drivertest.sh $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment8/assignment-test.sh $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment8/sockettest.sh $(TARGET_DIR)/usr/bin
+
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
